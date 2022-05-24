5e34f8f795d646bc65349c7f041d33940ac2195a
Lionelhg777 <105374812+Lionelhg777@users.noreply.github.com>
2022-05-24 17:16:12 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,9 @@ public class Greeter {
 
   }
 
-//TODO: Add javadoc comment
+  /**
+   * @return: This is a constructor.
+   */
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
