f50c0c541adbac56eaba0f75a04bd6c1b72f3fe4
Lionelhg777 <105374812+Lionelhg777@users.noreply.github.com>
2022-05-24 09:59:12 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,8 @@ public class Greeter {
 
   }
 
-  //Add javadoc comment
-  public String greet(final String someone) {
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
